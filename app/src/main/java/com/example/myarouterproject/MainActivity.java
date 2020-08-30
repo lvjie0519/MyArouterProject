7f70eced6be2d314d3f95e859058109f942064f2
lvjie1 <lvjie1@xiaomi.com>
2020-08-30 18:01:47 +0800

    LiveData 简单使用

--- app/src/main/java/com/example/myarouterproject/MainActivity.java
+++ app/src/main/java/com/example/myarouterproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myarouterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.router.MyRouter;
@@ -10,13 +13,40 @@ import com.example.router.MyRouter;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * LiveData  具有handler的能力， 并且能够防止内存泄漏问题
+     */
+    private MutableLiveData<String> mLiveData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+    }
+
+    private void initData(){
+        mLiveData = new MutableLiveData<>();
+
+        mLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Log.i("MainActivity", "onChanged s --->"+s);
+            }
+        });
     }
 
     public void openLoginActivity(View view) {
         MyRouter.getInstance().jumpActivity("login/LoginActivity", null);
+    }
+
+    public void onClickLivaData(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mLiveData.postValue("1111111111");
+            }
+        }).start();
     }
 }
